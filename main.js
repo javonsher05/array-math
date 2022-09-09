e0e9af9604b846245823f17eb024332dbafa1579
javonsher05 <110827599+javonsher05@users.noreply.github.com>
2022-09-09 23:14:44 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,297 @@
+//    ARRAY METHODS //
+
+// 1-misol
+
+// let fruits = [
+//     "olma" ,
+//     "kiwi",
+//     "xurmo",
+//     "uzum",
+//     "shoftoli",
+//     "apilsin",
+//     "ananas",
+//     "anjir",
+//     "avakado",
+//     "kakos",
+//     "mando",
+//     "tarvuz",
+//     "qovun",
+//     "malina",
+//     "gilos"
+// ]
+// document.getElementById("demo").innerHTML = fruits.join(" , ");
+
+// 2-misol
+
+// let fruits = [
+//     "olma" ,
+//     "kiwi",
+//     "xurmo",
+//     "uzum",
+//     "shoftoli",
+//     "apilsin",
+//     "ananas",
+//     "anjir",
+//     "avakado",
+//     "kakos",
+//     "mando",
+//     "tarvuz",
+//     "qovun",
+//     "malina",
+//     "gilos"
+// ]
+// document.getElementById("demo").innerHTML=fruits.toString()
+
+// 3-misol
+
+// let fruits = [
+//     "olma",
+//     "kiwi",
+//     "xurmo",
+//     "uzum",
+//     "shoftoli",
+//     "apilsin",
+//     "ananas",
+//     "anjir",
+//     "avakado",
+//     "kakos",
+//     "mando",
+//     "tarvuz",
+//     "qovun",
+//     "malina",
+//     "gilos"
+// ]
+// document.getElementById("demo1").innerHTML = fruits;
+// fruits.pop()
+// document.getElementById("demo2").innerHTML = fruits;
+// fruits.pop()
+
+// 4-misol
+
+// let fruits = [
+//     "olma",
+//     "kiwi",
+//     "xurmo",
+//     "uzum",
+//     "shoftoli",
+//     "apilsin",
+//     "ananas",
+//     "anjir",
+//     "avakado",
+//     "kakos",
+//     "mando",
+//     "tarvuz",
+//     "qovun",
+//     "malina",
+//     "gilos"
+// ]
+// document.getElementById("demo1").innerHTML = fruits.pop()
+// document.getElementById("demo2").innerHTML = fruits
+
+// 5-misol
+
+// let fruits = [
+//     "olma",
+//     "kiwi",
+//     "xurmo",
+//     "uzum",
+//     "shoftoli",
+//     "apilsin",
+//     "ananas",
+//     "anjir",
+//     "avakado",
+//     "kakos",
+//     "mando",
+//     "tarvuz",
+//     "qovun",
+//     "malina",
+//     "gilos"
+// ]
+// document.getElementById("demo1").innerHTML = fruits;
+// fruits.push("salom")
+// document.getElementById("demo2").innerHTML = fruits
+
+// 6-misol
+
+// let fruits = [
+//     "olma",
+//     "kiwi",
+//     "xurmo",
+//     "uzum",
+//     "shoftoli",
+//     "apilsin",
+//     "ananas",
+//     "anjir",
+//     "avakado",
+//     "kakos",
+//     "mando",
+//     "tarvuz",
+//     "qovun",
+//     "malina",
+//     "gilos"
+// ]
+// document.getElementById("demo1").innerHTML=fruits.push("salom")
+// document.getElementById("demo2").innerHTML=fruits
+
+// 7-misol
+
+// let fruits = [
+//     "olma",
+//     "kiwi",
+//     "xurmo",
+//     "uzum",
+//     "shoftoli",
+//     "apilsin",
+//     "ananas",
+//     "anjir",
+//     "avakado",
+//     "kakos",
+//     "mando",
+//     "tarvuz",
+//     "qovun",
+//     "malina",
+//     "gilos"
+// ]
+// document.getElementById("demo1").innerHTML = fruits
+// fruits.shift()
+// document.getElementById("demo2").innerHTML = fruits
+
+// 8-misol
+
+// let fruits = [
+//     "olma",
+//     "kiwi",
+//     "xurmo",
+//     "uzum",
+//     "shoftoli",
+//     "apilsin",
+//     "ananas",
+//     "anjir",
+//     "avakado",
+//     "kakos",
+//     "mando",
+//     "tarvuz",
+//     "qovun",
+//     "malina",
+//     "gilos"
+// ]
+// document.getElementById("demo1").innerHTML = fruits.shift()
+// document.getElementById("demo2").innerHTML = fruits
+
+// 9-misol
+
+// let fruits = [
+//     "olma",
+//     "kiwi",
+//     "xurmo",
+//     "uzum",
+//     "shoftoli",
+//     "apilsin",
+//     "ananas",
+//     "anjir",
+//     "avakado",
+//     "kakos",
+//     "mando",
+//     "tarvuz",
+//     "qovun",
+//     "malina",
+//     "gilos"
+// ]
+// document.getElementById("demo1").innerHTML = fruits
+// fruits.unshift("limon")
+// document.getElementById("demo2").innerHTML = fruits
+
+// 10-misol
+
+// let fruits = [
+//     "olma",
+//     "kiwi",
+//     "xurmo",
+//     "uzum",
+//     "shoftoli",
+//     "apilsin",
+//     "ananas",
+//     "anjir",
+//     "avakado",
+//     "kakos",
+//     "mando",
+//     "tarvuz",
+//     "qovun",
+//     "malina",
+//     "gilos"
+// ]
+// document.getElementById("demo1").innerHTML=fruits.unshift("limon")
+// document.getElementById("demo2").innerHTML=fruits
+
+// 11-misol
+
+// let fruits = [
+//     "olma",
+//     "kiwi",
+//     "xurmo",
+//     "uzum",
+//     "shoftoli",
+//     "apilsin",
+//     "ananas",
+//     "anjir",
+//     "avakado",
+//     "kakos",
+//     "mando",
+//     "tarvuz",
+//     "qovun",
+//     "malina",
+//     "gilos"
+// ]
+// document.getElementById("demo1").innerHTML=fruits
+// fruits[0]="kiwi"
+// document.getElementById("deno").innerHTML=fruits
+
+// 12-misol
+
+// let fruits = [
+//     "olma",
+//     "kiwi",
+//     "xurmo",
+//     "uzum",
+//     "shoftoli",
+//     "apilsin",
+//     "ananas",
+//     "anjir",
+//     "avakado",
+//     "kakos",
+//     "mando",
+//     "tarvuz",
+//     "qovun",
+//     "malina",
+//     "gilos"
+// ]
+// document.getElementById("demo1").innerHTML=fruits
+// fruits[fruits.length]="kiwi"
+// document.getElementById("demo2").innerHTML=fruits
+
+// 13-misol
+
+// let fruits = [
+//     "olma",
+//     "kiwi",
+//     "xurmo",
+//     "uzum",
+//     "shoftoli",
+//     "apilsin",
+//     "ananas",
+//     "anjir",
+//     "avakado",
+//     "kakos",
+//     "mando",
+//     "tarvuz",
+//     "qovun",
+//     "malina",
+//     "gilos"
+// ]
+// document.getElementById("demo1").innerHTML =
+//     fruits[0];
+
+// delete fruits[0];
+
+// document.getElementById("demo2").innerHTML =
+//     fruits[0];
